27cf41e63e05b3214d9aebd4285d4b23ea429f86
rashidshafeev <rashidshafeev@gmail.com>
2021-06-29 17:56:30 +0300

    Router 2

---
+++ scripts/start.js
@@ -0,0 +1,43 @@
+const Hapi = require('@hapi/hapi');
+const path = require('path')
+
+const port = process.env.PORT ||3000;
+
+const FILES = /\.(js|js.map|woff|woff2|svg|bmp|jpg|jpeg|gif|png|ico)(\?v=\d+\.\d+\.\d+)?$/;
+
+const PATH = {
+    '/' : 'index.html'
+}
+
+const init = async () => {
+    const server = Hapi.server({
+        port,
+    });
+
+    await server.register(require('@hapi/inert'));
+
+    server.route({
+        method: 'GET',
+        path: '/{path*}',
+        handler: (request, h) => {
+            console.log(request.path);
+
+            if(FILES.test(request.path)) {
+                return h.file(path.join(process.cwd(), 'dist', request.path))
+            };
+
+            return h.file(path.join(process.cwd(), 'dist', PATH[request.path]));
+        }
+    });
+
+    await server.start();
+
+    console.log('Server running', server.info.uri );
+}
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
