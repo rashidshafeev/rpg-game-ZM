d31f0a582eac2b304f5cca2bbd911ab0da6594e4
rashidshafeev <rashidshafeev@gmail.com>
2021-07-16 10:42:46 +0300

    Homework-3

--- src/client/ClientGame.js
+++ src/client/ClientGame.js
@@ -3,16 +3,24 @@ import ClientWorld from './ClientWorld';
 
 import sprites from '../configs/sprites';
 import levelCfg from '../configs/world.json';
+import gameObjects from  '../configs/gameObjects.json';
 
 class ClientGame {
   constructor(cfg) {
+
     Object.assign(this, {
       cfg,
+      gameObjects,
+      player: null
     });
 
     this.engine = this.createEngine();
     this.map = this.createWorld();
     this.initEngine();
+  }
+
+  setPlayer(player) {
+    this.player = player;
   }
 
   createEngine() {
@@ -23,11 +31,52 @@ class ClientGame {
     this.engine
       .loadSprites(sprites)
       .then(() => {
+        this.map.init();
         this.engine.on('render', (_, time) => {
-          this.map.init();
+          this.map.render(time);
         })
         this.engine.start();
+        this.initKeys();
       });
+  }
+
+  initKeys() {
+    this.engine.input.onKey({
+      ArrowLeft: (keydown) => {
+        if (keydown) {
+          this.player.moveByCellCoord(-1, 0, (cell) => {
+            return cell.findObjectsByType('grass').length;
+          })
+        }
+      }
+    })
+      this.engine.input.onKey({
+      ArrowRight: (keydown) => {
+        if (keydown) {
+          this.player.moveByCellCoord(1, 0, (cell) => {
+            return cell.findObjectsByType('grass').length;
+          })
+        }
+      }
+    })
+    this.engine.input.onKey({
+      ArrowDown: (keydown) => {
+        if (keydown) {
+          this.player.moveByCellCoord(0, 1, (cell) => {
+            return cell.findObjectsByType('grass').length;
+          })
+        }
+      }
+    })
+    this.engine.input.onKey({
+      ArrowUp: (keydown) => {
+        if (keydown) {
+          this.player.moveByCellCoord(0, -1, (cell) => {
+            return cell.findObjectsByType('grass').length;
+          })
+        }
+      }
+    })
   }
 
   createWorld() {
